8fd6a5d6cca3d1373d2d3ee89254e8d4f70880ec
michalmielc <102482708+michalmielc@users.noreply.github.com>
2023-03-25 16:22:19 +0100

    layout cd.

--- js_script.js
+++ js_script.js
@@ -2,15 +2,21 @@ let resetButton = document.querySelector("#reset");
 let settingsButton = document.querySelector("#opt_submit");
 let hiddenSections =  document.querySelectorAll(".hide");
 let optionSection = document.querySelector(".options"); 
-let boardHeight = "";
-let height=0;
-let boardWidth = "";
-let width=0;
-let minesAmount = "";
-let mines =0;
+let field = document.querySelector(".tile");
+let boardHeight = 0;
+let boardWidth = 0;
+let minesAmount = 0;
 let isGameActice = false;
+let maxHeightSize = 20;
+let maxWidthSize =20;
+let maxMinesAmount = 0;
+let board = [];
+ /** @type {HTMLDivElement} */
+let tiles ="";
 
 //////////// WALIDACJA INPUTÓW --------------------------------------------
+// ---- HEIGHT ------------------------------------------------------------ 
+
 let  inputValueHeight = document.getElementById("board_height").value;
 let  inputHeight =  document.getElementById("board_height");
 
@@ -19,28 +25,126 @@ inputHeight.addEventListener('beforeinput',e=>{
     inputValueHeight = document.getElementById('board_height').value;
 
         if (!(/^[0-9]+$/.test(e.data))){
-            console.log("beforeinput BŁĄD!");
+ 
            e.preventDefault();
         }
     })
 
 inputHeight.addEventListener('input', e=>{
 
+
     if (!(/^[0-9]+$/.test(e.data))){
-        console.log("input BŁĄD!");
+
         e.preventDefault();
     }
 
-    // DOPISAĆ ZEROWANIE POLA 
     else  {
         inputValueHeight = document.getElementById('board_height').value;
-        if (inputValueHeight>100) {
-            console.log("zmiana");
-            inputValueHeight = 100;
-            inputHeight.value=100;
+        if (inputValueHeight>maxHeightSize ||inputValueHeight<=0 ) {
+            inputValueHeight = maxHeightSize;
+            inputHeight.value=maxHeightSize;
+        }
+        const event = new Event('click');
+        resetMines.dispatchEvent(event);
+    }
+
+})
+
+let  heightReset =  document.getElementById("resetHeight");
+
+heightReset.addEventListener('click',e=>{
+    inputHeight.value = maxWidthSize;
+    const event = new Event('click');
+    resetMines.dispatchEvent(event);
+
+})
+// ---- WIDTH  ------------------------------------------------------------ 
+
+let  inputValueWidth = document.getElementById("board_width").value;
+let  inputWidth =  document.getElementById("board_width");
+
+inputWidth.addEventListener('beforeinput',e=>{
+
+    inputValueWidth = document.getElementById('board_width').value;
+
+        if (!(/^[0-9]+$/.test(e.data))){
+           e.preventDefault();
+        }
+    })
+
+inputWidth.addEventListener('input', e=>{
+
+    if (!(/^[0-9]+$/.test(e.data))){
+        e.preventDefault();
+    }
+
+    else  {
+        inputValueWidth = document.getElementById('board_width').value;
+        if (inputValueWidth>maxWidthSize ||inputValueWidth<=0 ) {
+            inputValueWidth = maxWidthSize;
+            inputWidth.value = maxWidthSize;
+        }
+        const event = new Event('click');
+        resetMines.dispatchEvent(event);
+    }
+
+})
+
+let  widthReset =  document.getElementById("resetWidth");
+
+widthReset.addEventListener('click',e=>{
+    inputWidth.value = maxWidthSize;
+    const event = new Event('click');
+    resetMines.dispatchEvent(event);
+})
+//--------------------------------------------------------------------------
+
+// ---- MINES ------------------------------------------------------------ 
+
+let  inputValueMines = document.getElementById("mines_amount").value;
+let  inputMines =  document.getElementById("mines_amount");
+let  inputMinesPerc =  document.getElementById("mines_perc");
+
+inputMines.addEventListener('beforeinput',e=>{
+
+    inputValueMines = document.getElementById('mines_amount').value;
+
+        if (!(/^[0-9]+$/.test(e.data))){
+           e.preventDefault();
+        }
+    })
+
+inputMines.addEventListener('input', e=>{
+
+    if (!(/^[0-9]+$/.test(e.data))){
+        e.preventDefault();
+    }
+
+    else  {
+        inputValueMines = document.getElementById('mines_amount').value;
+        let h = document.getElementById("board_height").value;
+        let w = document.getElementById("board_width").value;
+        let perc = document.getElementById("mines_perc");
+        maxMinesAmount = h*w;
+
+        if (inputValueMines>maxMinesAmount ||inputValueMines<=0 ) {
+            inputValueMines = maxMinesAmount;
+            inputMines.value = maxMinesAmount;
+        }
+
+        else {
+
+            perc.value = parseInt(100*(inputValueMines/maxMinesAmount));
         }
     }
 
+})
+
+let  resetMines =  document.getElementById("resetMines");
+resetMines.addEventListener('click',e=>{
+    inputMines.value = 0;
+    inputMinesPerc.value=0;
+    inputValue = 0;
 })
 
 /////////// ----------------------------------------------------------------
@@ -59,32 +163,31 @@ function resetBoard() {
     while (myNode.lastElementChild) {
       myNode.removeChild(myNode.lastElementChild);}
 
+    window.location.reload();    
 }
-
 
 // MANIPULOWANIE SEKCJAMI 
 // USTAWIENIA PLANSZY
 settingsButton.addEventListener("click", function() {
     hiddenSections.forEach(el=>{ el.classList.remove("hide");});
     optionSection.classList.add("hide");
-    height = document.getElementById("board_height");
-    boardHeight  = height.value;
-    width = document.getElementById("board_width");
-    boardWidth  = width.value;
-    mines = document.getElementById("mines_amount");
-    mines_amount  = mines.value;
+    boardHeight = document.getElementById("board_height").value;
+    boardWidth = document.getElementById("board_width").value;
+    minesAmount = document.getElementById("mines_amount").value;
     
     //RYSOWANIE PLANSZY
-    drawBoard(boardHeight,boardWidth,mines_amount);
+    assignBoard(boardHeight,boardWidth, board);
+    assignMines(minesAmount,boardHeight,boardWidth, board);
+    assignValuesToBoard(boardHeight,boardWidth,board);
+    drawBoard(boardHeight,boardWidth);
     isGameActice=true;
 
 });
 
 //RYSOWANIE PLANSZY
-function drawBoard(boardHeight,boardWidth,mines_amount){
+function drawBoard(boardHeight,boardWidth){
   
     let boardBox = document.getElementById("fields");
-
 
     for (let i =0; i<boardWidth;i++) {
 
@@ -93,8 +196,16 @@ function drawBoard(boardHeight,boardWidth,mines_amount){
         for (let j =0; j<boardHeight;j++) {
 
             const tile = document.createElement("div");
+            const tileBox = document.createElement("div");
             tile.className="tile";
-            tile.setAttribute("id",i+"_"+j);
+            tile.setAttribute("id",j+"_"+i);
+            tileBox.className="tileBox";
+            
+
+            //tileBox.innerText = board[j][i];
+            tile.appendChild(tileBox);
+
+            tileBox.style.visibility="hidden";
             //DODAWANIE POLA
             // <div class="tile" id="i_j"></div>
            row.appendChild(tile); 
@@ -104,230 +215,169 @@ function drawBoard(boardHeight,boardWidth,mines_amount){
         boardBox.appendChild(row);
     }
 
-}
+  
+        //################################################################
+        // OBSŁUGA MYSZY LPM I PPM
+       
+        tiles = Array.from(document.querySelectorAll(".tile"));
 
-//SPRAWDZENIE WARTOŚCI
-function checkValues(id_value)
+        // // PODPIĘCIE ZDARZENIA DO KAFELKÓW LPM
+        tiles.forEach ( (tile,index)=> {
+            tile.addEventListener("click",()=>uncoverTile(tile,index)); 
+        });
+
+         // // PODPIĘCIE ZDARZENIA DO KAFELKÓW PPM
+          tiles.forEach ( (tile,index)=> {
+            tile.addEventListener("contextmenu",(e)=>{
+            e.preventDefault();
+            markTile(tile,index);})});
+    }
+
+// STWORZENIE GLOBALNEJ TABLICY
+function assignBoard(boardHeight,boardWidth,board)
 {
-   
+    for (let i = 0; i<boardHeight; i++) {
+        let row = [];
+        for (let j = 0; j<boardWidth; j++) {
+            row[j]='';
+        }
+        board.push(row);
+    }
+}
+
+// PRZYPISANIE WARTOŚCI POLA W ZALEZNOŚCI OD ILOŚCI MIN
+function assignValuesToBoard(boardHeight,boardWidth,board){
+    for (let i = 0; i<boardHeight; i++) {
+        for (let j = 0; j<boardWidth; j++) {
+
+           let minesValueNearby = 0;
+
+           if (board[i][j]==='M'){
+            continue;
+           }
+
+           else {
+            for (let m = -1; m<2; m++) {
+                for (let n= - 1; n<2; n++) {
+                    if ((i+m)>=0 && (i+m)<boardHeight && (j+n)>=0 && (j+n)<boardWidth) {
+            
+                        if (board[i+m][j+n]==='M'){
+                            minesValueNearby=minesValueNearby+1;
+                           }
+                       
+                    } 
+                }
+            }
+
+            //console.log(board[i,j]+ " liczba min " + minesValueNearby);
+            board[i][j]=minesValueNearby;
+           }
+
+
+        }
+}
+}
+//FUNKCJA ROZMIESZAJĄCA LOSOWO MINY
+function assignMines(minesAmount,boardHeight,boardWidth, board){
+  
+     let tempMinesAmount = minesAmount;
+     let freeIndexes = [];
+    
+     for ( let k=0; k<boardHeight*boardWidth; k++ )
+     {
+        freeIndexes[k]=k;
+     }
+
+     while(tempMinesAmount>0)
+     {
+      
+        //USTALENIE AKTUALNEJ LICBZY WOLLNYCH MIEJSC
+        const y = freeIndexes.length;
+        //LOSOWANIE INDEKSU
+        const rnd = Math.floor(Math.random()*y);
+        const x = freeIndexes[rnd];
+        const i = getMultiple(x,boardWidth);
+        const j = x%boardWidth;
+    
+        board[i][j]="M";
+        // USUNIĘCIE EL X
+        freeIndexes.splice(rnd,1);
+        tempMinesAmount=tempMinesAmount-1;
+     }
+}
+
+// ZWRACA WIELOKROTNOŚĆ
+function getMultiple(a,b) {
+
+    let temp = 0;
+    b = Number(b);
+    a = Number(a);
+    
+    while (b<=a){
+        temp=temp+1;
+        a=a-b;
+    }
+
+  
+    return temp;
+}
+
+//ODSŁONIĘCIE POLA
+function uncoverTile(tile,index) {
+
+   let tileBox = tile.querySelector(".tileBox"); 
+
+   if(!tileBox.classList.contains("locked")){
+        const i = getMultiple(index,boardWidth);
+        const j = index%boardWidth;
+        tileBox.innerText = board[i][j];
+        tileBox.style.visibility="visible";
+        tileBox.classList.add("locked");
+        if(board[i][j]==="M") {
+                endGame();
+        }
+    }
+}
+
+//ZAZNACZANIE POLA
+function markTile(tile,index) {
+
+    let tileBox = tile.querySelector(".tileBox"); 
+    if (!tileBox.classList.contains("locked")){
+
+        if (tileBox.innerText === "?") {
+            tileBox.style.background="none";
+            tileBox.style.visibility="hidden";
+        }
+
+        else 
+        {
+     
+           tileBox.style.background="yellow";
+            tileBox.style.visibility="visible";
+            tileBox.innerText = "?";
+        }
+    }
 
 }
 
+//KONIEC GRY
+function endGame(){
+    isGameActice=true;
+    tiles.forEach((tile,index)=>{
 
-
-// ////RESET PLANSZY
-        // function resetBoard() {
-        //     board =['','','','','','','','',''];
-        //     isGameActice=false;
-        //     tiles.forEach(tile=>{
-        //         tile.innerText="";
-        //     });
-        //     message.innerText="";
-        //     userPlayer ="-";
-        //     pcPlayer = "-";
-        // }
-
-
-
-// let message =document.querySelector(".message");
-
-// const tiles = Array.from(document.querySelectorAll(".tile"));
-// let fields = document.getElementById("fields").children;
-// let minMax = true;
-// let board =['','','','','','','','',''];
-// let boardMinMax = ['','','','','','','','',''];
-
-
-
-
-
-
-// // PROTOTYP PIERWSZEGO RUCHU
-// // function ifFirstMove(){
-// //     let firstMove = true;
-// //     for ( let i =0; i<=8;i++) {
-// //         if (board[i]!==""){
-// //              firstMove = false;   
-// //              break;
-// //         }
-// //     }
-
-// //     return firstMove;
-// // }
-
-// // //RUCH KOMPUTERA
-// function movePc(minMax){
-
-//     let move=-1;
-    
-//     if(minMax){
-
-//                 boardMinMax= Array.from(board);
-//                 move = startMinMax(boardMinMax);
-//         }
-    
-
-//     else {
-//         move= randomPcMOve();
-//     }
-
-//         updateBoard(move,pcPlayer);
-//         fields[move].innerText=pcPlayer;
-//         checkResult();
-    
-// }
-
-	
-
-// ////////////////////////////////////////////////////////////
-// ////////////////////////////////////////////////////////////
-
-
-// // // PODPIĘCIE ZDARZENIA DO KAFELKÓW
-// tiles.forEach ( (tile,index)=> {
-//     tile.addEventListener("click",()=>playerMove(tile,index)); 
-
-// });
-
-// // // RUCH GRACZA
-// const  playerMove =(tile,index ) => {
-//     if (isTileEmpty(tile) && isGameActice) {
-//             tile.innerText = userPlayer;
-//             updateBoard(index,userPlayer);
-//             checkResult();
-//             if (isGameActice) {
-//                 movePc(minMax)
-//             }
-//         return; }
-
-// };
-
-// //// WALIDACJA KAFELKA CZY JEST ZAJĘTY
-// const isTileEmpty = (tile) => {
-//     if(tile.innerText==="X" || tile.innerText==="O")
-//     {
-//         return false;
-//     }
-
-//     return true;};
-     
-// //// SPRAWDZENIE WARUNKÓW GRY    
-// function checkResult(){
-
-//     if (winningVariants()) {
-//         isGameActice=false;
-//         return;
-//     }
-
-//     if (!board.includes('')) {
-//        message.innerText="TIE";
-//        isGameActice=false;
-//        return;
-//     }
-// }
-
-// // SPRAWDZENIE WARIANTÓW
-// function winningVariants() {
-//     let winning = false;
-//     const winningConditions  = [
-//                 [0,1,2],
-//                 [3,4,5],
-//                 [6,7,8],
-//                 [0,3,6],
-//                 [1,4,7],
-//                 [2,5,8],
-//                 [0,4,8],
-//                 [2,4,6]];
-
-//     for ( let i=0; i<8;i++) {
-//         const win = winningConditions[i];
-//         const field1 = board[win[0]];
-//         const field2 = board[win[1]];
-//         const field3 = board[win[2]];
-
-//         if (( field1===field2) && (field2 === field3) && (field1!=="")) {
-//             if (field1==="X") {
-//                 message.innerText = "WINNING BY X";
-//                 winning =  true;
-//                }
-//             else {
-//                 message.innerText = "WINNING BY O";
-//                 winning =  true;
-//             }   
-//         }
-//     }
-
- 
-//     return winning;
-// }
-
-// // SPRAWDZENIE WARIANTÓW ZWYCIESTWA REMISU PORAŻKI
-// function winningVariantsMinMax(boardMinMax) {
-//     let winning = false;
-//     const winningConditions  = [
-//                 [0,1,2],
-//                 [3,4,5],
-//                 [6,7,8],
-//                 [0,3,6],
-//                 [1,4,7],
-//                 [2,5,8],
-//                 [0,4,8],
-//                 [2,4,6]];
-
-//     for ( let i=0; i<8;i++) {
-//         const win = winningConditions[i];
-//         const field1 = boardMinMax[win[0]];
-//         const field2 = boardMinMax[win[1]];
-//         const field3 = boardMinMax[win[2]];
-
-//         if (( field1===field2) && (field2 === field3) && (field1!=="")) {
-//             if (field1==="X") {
-//                 winning =  true;
-//                }
-//             else {
-//                 winning =  true;
-//             }   
-//         }
-//     }
-
- 
-//     return winning;
-// }
-
-
-// ////LOSOWY RUCH KOMPA
-// function randomPcMOve() {
+        let tileBox = tile.querySelector(".tileBox"); 
    
-//     let freeTile=-1;
-//     let indx =-1;
-//     const tabFreeFields = [];
-//     for ( let i =0; i<=8;i++) {
-//         if (board[i]===""){
-//             tabFreeFields.push(i);
-//         }
-//     }
-//     indx = randomNumber(0,tabFreeFields.length-1);
-//     return freeTile = tabFreeFields[indx] ;
-// }
+        tileBox.style.visibility="visible";
+    
+        const i = getMultiple(index,boardWidth);
+        const j = index%boardWidth;
+        tileBox.innerText = board[i][j];
 
-// //// GENEROWANIE LOSOWEJ LICZBY
-// function randomNumber(min, max) {
-   
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     if (min==max) {
-//         return 0;
-//     }
-//     else{
-//         const indx = Math.floor(Math.random() * (max - min + 1) + min);
-//         return indx ;
-//     }
-// };
-
-// ////UPDATE TABLICY
-// const updateBoard = (index,player) => {
-//     board[index] = player;
-
-// }
-
+        if (tileBox.innerText ==="M")
+        {
+            tileBox.style.background="red";
+        }
+    });
+}
 
